4c8113661d5289d4809bbefbb72e51da6faf1e1a
tom <tom@exmple.com>
2018-06-12 15:54:42 +0000

    deleted unnecessary files

--- prime_customers1.java
+++
@@ -1,8 +0,0 @@
-TOM java class fir prime customers.
-
-Adding new lines - tom
-<the lines which is missed>
-
-New function for prime customers.
-
-JERRY a new function to calculate the delivery time 
